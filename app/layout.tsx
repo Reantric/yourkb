6243610402985d56b2c39ef90dd0b55d0b3e91d8
Eric Zou <ezou626@gmail.com>
2024-09-05 05:24:43 -0400

    remove deploy to vercel button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import HeaderAuth from "@/components/header-auth";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
@@ -35,9 +34,6 @@ export default function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>YourKB</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
                   </div>
                   <HeaderAuth />
                 </div>
