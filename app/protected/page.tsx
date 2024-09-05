c9373dcf70dad234065ebd82dcc28ec6ef6036f2
Eric Zou <ezou626@gmail.com>
2024-09-05 03:17:03 -0400

    get initial data and create record if not existing

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,9 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
+import CanvasGrid from "@/components/editorGrid";
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+
+const DEFAULT_BG = '#ffffff';
+const DEFAULT_FG = '#000000';
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -14,11 +16,40 @@ export default async function ProtectedPage() {
     return redirect("/sign-in");
   }
 
+  let fg_color;
+  let bg_color;
+  let value;
+
+  const { data, error } = await supabase
+  .from('kilobytes')
+  .select()
+  .eq('user_id', user.id);
+
+  if (!data || data.length === 0) {
+    await supabase
+    .from('kilobytes')
+    .insert({ 
+      id: 1,
+      bg_color: DEFAULT_BG,
+      fg_color: DEFAULT_FG,
+      value: '0'.repeat(1024),
+      user_id: user.id
+    });
+    fg_color = DEFAULT_FG;
+    bg_color = DEFAULT_BG;
+    value = '0'.repeat(1024);
+  } else {
+    fg_color = data[0].fg_color;
+    bg_color = data[0].bg_color
+    value = data[0].value;
+  }
+
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
         <h1 className="font-bold text-2xl mb-4">Your Kilobyte</h1>
       </div>
+      <CanvasGrid fg_color={fg_color} bg_color={bg_color} hexString={value}></CanvasGrid>
     </div>
   );
 }
