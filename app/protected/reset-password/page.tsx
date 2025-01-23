9958a916352154160840ee90ec489ff3db4b8084
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-23 01:34:07 -0500

    this too

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -9,6 +9,7 @@ export default async function ResetPassword({
 }: {
   searchParams: Promise<Message>;
 }) {
+  const message = await searchParams;
   return <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
     <h1 className="text-2xl font-medium">Reset password</h1>
     <p className="text-sm text-foreground/60">
@@ -33,6 +34,6 @@ export default async function ResetPassword({
     <SubmitButton formAction={resetPasswordAction}>
       Reset password
     </SubmitButton>
-    <FormMessage message={searchParams} />
+    <FormMessage message={message} />
   </form>;
 }
