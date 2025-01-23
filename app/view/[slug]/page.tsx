a6871231d62c263d9f11456d347d09eb7ecfc705
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-22 22:01:46 -0500

    remove redundant fragment

--- app/view/[slug]/page.tsx
+++ app/view/[slug]/page.tsx
@@ -14,16 +14,14 @@ export default async function ViewKilobyte({ params }: { params: { slug: string 
         .eq('id', slug);
 
   return (
-    <>
-      <div className="flex-1 flex flex-col gap-6 px-4">
-        {(data && data.length !== 0) ? 
-          <CanvasDisplay 
-            fg_color={data[0].fg_color} 
-            bg_color={data[0].bg_color} 
-            hexString={data[0].value}>
-            </CanvasDisplay> 
-            : <p>This KB seems to be missing.</p>}
-      </div>
-    </>
+    <div className="flex-1 flex flex-col gap-6 px-4">
+      {(data && data.length !== 0) ? 
+        <CanvasDisplay 
+          fg_color={data[0].fg_color} 
+          bg_color={data[0].bg_color} 
+          hexString={data[0].value}>
+          </CanvasDisplay> 
+          : <p>This KB seems to be missing.</p>}
+    </div>
   );
 }
