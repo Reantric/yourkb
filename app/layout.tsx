d248525f232cf11deea8b6b0bfb77441ae5b9ac8
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-23 01:27:09 -0500

    nevermind looks like i need this

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
