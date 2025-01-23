ac0d52afe530146d93abadc1749d42b9922b63fe
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-23 01:34:02 -0500

    make searchParams async

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label";
 export default async function ResetPassword({
   searchParams,
 }: {
-  searchParams: Message;
+  searchParams: Promise<Message>;
 }) {
   return <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
     <h1 className="text-2xl font-medium">Reset password</h1>
