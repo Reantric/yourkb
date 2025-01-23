1b4e3d368035e0e5348051388f82161d18d04d21
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-22 22:04:09 -0500

    style

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -50,11 +50,9 @@ export default async function ProtectedPage() {
     value = data[0].value;
   }
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-6">
-      <h1 className="text-4xl">Your Kilobyte</h1>
-      <CopyLinkButton id={id}></CopyLinkButton>
-      <Editor initBgColor={bg_color} initFgColor={fg_color} initHexString={value}></Editor>
-    </div>
-  );
+  return <div className="flex-1 w-full flex flex-col gap-6">
+    <h1 className="text-4xl">Your Kilobyte</h1>
+    <CopyLinkButton id={id}></CopyLinkButton>
+    <Editor initBgColor={bg_color} initFgColor={fg_color} initHexString={value}></Editor>
+  </div>;
 }
