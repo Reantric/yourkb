2d44f79bd94024e29b1f9a5d12c24daacca94c6b
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-23 01:27:38 -0500

    add changelog component

---
+++ app/changelog/page.tsx
@@ -0,0 +1,15 @@
+import { CHANGES } from './changelog';
+
+export default async function Changelog() {
+
+  return <div className="gap-6 px-4">
+    <ul className='float-left'>
+    {CHANGES.map(
+      (c, index) => <li key={index}>
+        <h2 className='font-bold text-xl'>{c.month}-{c.day}-{c.year}</h2>
+        <p>{c.description}</p>
+      </li>
+    )}
+    </ul>
+  </div>;
+}
