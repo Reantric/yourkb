2cc1098ae15a47863fc34050472060b20557cecb
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-22 22:00:56 -0500

    remove redundant main

--- app/view/[slug]/page.tsx
+++ app/view/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function ViewKilobyte({ params }: { params: { slug: string 
 
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <div className="flex-1 flex flex-col gap-6 px-4">
         {(data && data.length !== 0) ? 
           <CanvasDisplay 
             fg_color={data[0].fg_color} 
@@ -23,7 +23,7 @@ export default async function ViewKilobyte({ params }: { params: { slug: string 
             hexString={data[0].value}>
             </CanvasDisplay> 
             : <p>This KB seems to be missing.</p>}
-      </main>
+      </div>
     </>
   );
 }
