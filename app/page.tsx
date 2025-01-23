0d82a42d40f6e7d5b66d93d39ecccf654ed1a37d
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-01-23 00:27:35 -0500

    style

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,29 @@
 import Link from "next/link";
 
 export default async function Index() {
-  return (
-    <main>
-      <div className="flex flex-col gap-8 items-center">
-        <div className="flex justify-center items-center">
-        </div>
-        <h1 className="text-5xl lg:text-6xl !leading-tight mx-auto max-w-xl text-center">YourKB</h1>
-        <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-          The simplest way to display your creativity on the web.
-        </p>
-        <div className="flex flex-col gap-4 items-center">
-          <ul className="flex text-md text-center justify-between items-center space-x-20">
-            <li><Link
-                    href="/sign-up"
-                    className="hover:underline"
-                  >
-                    Create an account
-                  </Link></li>
-            <li>Draw something amazing</li>
-            <li><Link
-                    href="/view/1"
-                    className="hover:underline"
-                  >
-                   Share the link
-                  </Link></li>
-          </ul>
-        </div>
-      </div>
-    </main>
-  );
+  return <div className="flex flex-col gap-8 items-center">
+    <div className="flex justify-center items-center">
+    </div>
+    <h1 className="text-5xl lg:text-6xl !leading-tight mx-auto max-w-xl text-center">YourKB</h1>
+    <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+      The simplest way to display your creativity on the web.
+    </p>
+    <div className="flex flex-col gap-4 items-center">
+      <ul className="flex text-md text-center justify-between items-center space-x-20">
+        <li><Link
+                href="/sign-up"
+                className="hover:underline"
+              >
+                Create an account
+              </Link></li>
+        <li>Draw something amazing</li>
+        <li><Link
+                href="/view/1"
+                className="hover:underline"
+              >
+                Share the link
+              </Link></li>
+      </ul>
+    </div>
+  </div>;
 }
