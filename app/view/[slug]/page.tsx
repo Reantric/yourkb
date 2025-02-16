7b26578e86b955185096b570aea8098055e1c42e
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-02-16 00:13:10 -0500

    add link copier

--- app/view/[slug]/page.tsx
+++ app/view/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import CopyLinkButton from "@/components/linkGenerator";
 import CanvasDisplay from "@/components/viewer";
 
 import { createClient } from '@/utils/supabase/server';
@@ -14,7 +15,7 @@ export default async function ViewKilobyte({ params }: { params: Promise<{ slug:
         .eq('id', slug);
 
   return (
-    <div className="flex-1 flex flex-col gap-6 px-4">
+    <div className="flex-1 flex flex-col space-y-1">
       {(data && data.length !== 0) ? 
         <CanvasDisplay 
           fg_color={data[0].fg_color} 
@@ -22,6 +23,8 @@ export default async function ViewKilobyte({ params }: { params: Promise<{ slug:
           hexString={data[0].value}
           pixel_size={5}
         /> : <p>This KB seems to be missing.</p>}
+        {(data && data.length !== 0) ?
+         <CopyLinkButton id={data[0].id}></CopyLinkButton> : <></>}
     </div>
   );
 }
