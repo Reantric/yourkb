ac79b95fdaccd248cab300be207d5f5f7635cba8
Eric Zou <55918461+ezou626@users.noreply.github.com>
2025-08-21 18:12:23 -0400

    refactor logic to return early if there is an error getting an image

--- app/view/[slug]/page.tsx
+++ app/view/[slug]/page.tsx
@@ -1,30 +1,47 @@
 import CopyLinkButton from "@/components/linkGenerator";
 import CanvasDisplay from "@/components/viewer";
 
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from "@/utils/supabase/server";
 
-export default async function ViewKilobyte({ params }: { params: Promise<{ slug: string }> }) {
-
+export default async function ViewKilobyte({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
 
   const supabase = await createClient();
 
+  // not sure what protections supabase has against sql injection, so validate here
+  const desiredImageId = Number.parseInt(slug);
+
   const { data, error } = await supabase
-        .from('kilobytes')
-        .select()
-        .eq('id', slug);
+    .from("kilobytes")
+    .select()
+    .eq("id", desiredImageId);
+
+  if (error || !data) {
+    return (
+      <p>
+        An error occured when fetching this image. Please refresh the page to
+        try again.
+      </p>
+    );
+  }
+
+  if (data.length === 0) {
+    return <p>This kilobyte seems to be missing.</p>;
+  }
 
   return (
     <div className="flex-1 flex flex-col space-y-1">
-      {(data && data.length !== 0) ? 
-        <CanvasDisplay 
-          fg_color={data[0].fg_color} 
-          bg_color={data[0].bg_color} 
-          hexString={data[0].value}
-          pixel_size={5}
-        /> : <p>This KB seems to be missing.</p>}
-        {(data && data.length !== 0) ?
-         <CopyLinkButton id={data[0].id}></CopyLinkButton> : <></>}
+      <CanvasDisplay
+        fg_color={data[0].fg_color}
+        bg_color={data[0].bg_color}
+        hexString={data[0].value}
+        pixel_size={5}
+      />
+      <CopyLinkButton id={data[0].id}></CopyLinkButton>
     </div>
   );
 }
